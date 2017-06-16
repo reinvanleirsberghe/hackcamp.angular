7b3b0ce2c70bbf4eb271d8a3612d493554ab7290
Boubacar Barry <bouba@hackages.io>
2017-06-16 17:32:04 +0200

    Implement CLI, View, Binding Concepts:    - Bootstrapping your Angular app using Angular CLI    - Module (decorator)    - Component (decorator)    - Template Syntax & Binding Features:
      - Display all mock movies
        - Picture
        - Hover information
      - Display all mock categories
      - Filter by category
      - Make active a category when selected
      - Toggle Sidebar
      - Implement search in sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Category, Movie} from './types';
+import {getGenreId, movieContainsGenre} from './utils';
+import {PICTURES_CDN_URL} from './constant';
+import {movies} from './mocks/movies';
+import {categories} from './mocks/categories';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css',
+    '../assets/css/header.css',
+    '../assets/css/movie.css',
+  ]
 })
 export class AppComponent {
-  title = 'app';
+
+  logo = '../assets/images/logo.svg';
+
+  PICTURES_CDN_URL = PICTURES_CDN_URL;
+
+  movies: Movie[] = movies.slice(0, 50);
+  filteredMovies: Movie[] = movies.slice(0, 50);
+  categories: Category[] = categories;
+  searchValue: string;
+  navClosed = true;
+  hoverMovies = new Map();
+
+
+  selectTab(category) {
+    this.categories = this.categories.map(filter => {
+      filter.selected = filter.category === category;
+      return filter;
+    });
+    this.filterMovies();
+  };
+
+  closeSideBar() {
+    this.navClosed = true;
+  };
+
+  openSideBar() {
+    this.navClosed = false;
+  }
+
+  search(event) {
+    this.searchValue = event.target.value;
+    this.filterMovies();
+  };
+
+  filterMovies() {
+    const selectedCategory = this.categories.filter(f => f.selected)[0]
+      .category;
+
+    this.filteredMovies = this.movies
+      .filter(this.filterByCategory(selectedCategory))
+      .filter(this.filterByTitle(this.searchValue));
+  }
+
+  filterByCategory(selectedCategory: string) {
+    return (movie: Movie) => {
+      return selectedCategory === 'All' || movieContainsGenre(movie, getGenreId(selectedCategory));
+    }
+  }
+
+  filterByTitle(title: string) {
+    return (movie: Movie) => {
+      return !title || movie.title.toLowerCase().includes(title.toLowerCase());
+    }
+  }
+
+  toggleHoverForTheMovie(movieId: number) {
+    this.hoverMovies.set(movieId, !this.hoverMovies.get(movieId));
+  }
+
+  isMovieHovered(movieId: number) {
+    return this.hoverMovies.get(movieId);
+  }
+
+  shorten(text: string, limit: number) {
+    return text.split('').slice(0, limit).join('') + '...';
+  }
 }
+
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,6 +1,6 @@
 /** A few utility functions used accross the project **/
-import {genres} from './mocks/genres.js';
-import {movies} from './mocks/movies.js';
+import {genres} from './mocks/genres';
+import {movies} from './mocks/movies';
 import {Movie} from './types';
 
 export const getGenreName = (id: number) =>
