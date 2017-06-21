4021bbf67602815815912d8edeba6913693d9864
Boubacar Barry <bouba@hackages.io>
2017-06-21 19:37:11 +0200

    Add hackflix-backend

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/form-directives-ngrx/app.module';
+import { AppModule } from './app/routing-rx-advanced-service/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
