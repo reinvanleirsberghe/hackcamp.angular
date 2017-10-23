75b8139830bfaa834a447199cb15eb2ebaf8dbdc
Boubacar Barry <bouba@hackages.io>
2017-10-23 09:24:49 +0200

    Replace throw by of to move the error in the next channel to force people to handle the error and not kill the observable

--- src/app/routing-rx-advanced-service-module-architecture/core/api.service.ts
+++ src/app/routing-rx-advanced-service-module-architecture/core/api.service.ts
@@ -75,6 +75,6 @@ export class ApiService {
    * @returns {any}
    */
   private handleError(err: any): Observable<Error> {
-    return Observable.throw(new Error(err));
+    return Observable.of(new Error(err));
   }
 }
